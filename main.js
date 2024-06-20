5e2bb2650de996148bd7aebea8f109fec50f94b5
SayonaSinha <110324063+SayonaSinha@users.noreply.github.com>
2024-06-20 12:00:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("You are in Console Screen")
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+
+var a = 15;
+var b = 10;
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var add = a+b;
+console.log(add);
+
+var subtract = a-b;
+console.log(subtract);
+
+var multiplication = a*b;
+console.log(multiplication);
+
+var divide = a/b;
+console.log(divide);
